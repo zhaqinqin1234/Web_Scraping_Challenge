6940f6d37d41ed2ea5f10a9dfb753a1356233942
qinqin zha <qinqin.zha@gmail.com>
2020-09-20 19:26:52 -0700

    partial done

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_craigslist
+import scrape_mars
+import pandas as pd
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
 mongo = PyMongo(app)
 
 # Or set inline
@@ -14,15 +15,15 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update({}, listings_data, upsert=True)
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,5 +1,8 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import time
+import os
+import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -11,16 +14,63 @@ def init_browser():
 
 def scrape():
     browser = init_browser()
-    listings = {}
+    mars = {}
 
-    url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
+    url ="https://mars.nasa.gov/news/"
     browser.visit(url)
+    time.sleep(1)
 
     html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    soup = bs(html, "html.parser")
+    # Retrieve latest elements that contain title and paragraph
+    slide = soup.select_one('li.slide')
+    news_title = slide.find("div", class_="content_title").get_text()
+    news_para= slide.find("div", class_="article_teaser_body").get_text()
+    mars["news_title"] = news_title
+    mars["news_para"] = news_para
+    # featured image infor
+    img_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(img_url)
+    browser.links.find_by_partial_text('FULL IMAGE').click()
+    browser.links.find_by_partial_text('more info').click()
+    html = browser.html
+    img_soup = bs(html, 'html.parser')
+    imag = img_soup.find("figure", class_="lede").a['href']
+    imag_url = f'https://www.jpl.nasa.gov{imag}'
+    mars["img_url"]= img_url
 
-    listings["headline"] = soup.find("a", class_="result-title").get_text()
-    listings["price"] = soup.find("span", class_="result-price").get_text()
-    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+    # Mars facts
+    url ="https://space-facts.com/mars/"
+    browser.visit(url)
 
-    return listings
+    mars_table = pd.read_html(url)[0]
+    mars_table.columns=['Description', 'Value']
+    mars_table.set_index('Description', inplace=True)
+    mars_table.to_html(classes="table table-striped")
+
+
+    # hemisphere infor
+    hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemi_url)
+    time.sleep(1)
+    hemi_title_img =[]
+    # get a litst of all the hemispheres
+    links = browser.find_by_css("a.product-item h3")
+
+# loop through the links, click the links and sample anchor and retrun href
+    for i in range(len(links)):
+        hemisphere ={}
+        browser.find_by_css("a.product-item h3")[i].click()
+        hemisphere['img_url']=browser.find_link_by_text('Sample').first['href']
+        hemisphere['img_title']=browser.find_by_css("h2.title").text
+        
+        #Append hemisphere dictionay to hemi list
+        hemi_title_img.append(hemisphere)
+        # navigate backwards
+        browser.back()
+
+    mars["hemi_title_img"] = hemi_title_img
+
+    return mars
+
+  
